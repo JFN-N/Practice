97d124bb86251bc400f23da9053962634bd031de
EC2 Default User <staiju77@gmail.com>
2023-09-07 11:37:51 +0000

    計算

--- calculate.php
+++ calculate.php
@@ -6,3 +6,24 @@ $k=$i**3;
 
 echo $k;
 echo "\n";
+
+// 値を入れる
+$bool_t = true;
+$bool_f = false;
+var_dump($bool_t);
+var_dump($bool_f);
+
+// trueやfalseは、大文字で書いてもよい
+$bool_t_large = TRUE;
+var_dump($bool_t_large);
+
+// 数字
+$num_string = '999';
+var_dump($num_string);
+
+// 数値
+$num_int = 999;
+var_dump($num_int);
+
+define('TEST_FLOAT',1.23);
+var_dump(TEST_FLOAT);

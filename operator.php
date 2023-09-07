7476c067077dd56d4387c96bb154e3b65549f991
EC2 Default User <staiju77@gmail.com>
2023-09-07 12:00:31 +0000

    実装

--- operator.php
+++ operator.php
@@ -15,3 +15,12 @@ $s="php";
 var_dump($s==="PHP");
 var_dump($s==="php");
 var_dump($s=="php");
+
+$f = 0.3;
+var_dump($f === 0.3);
+
+//
+echo "\n";
+echo "0.1+0.2 と 0.3 を高精度のprintfで表示\n";
+printf("0.1+0.2: %.2f \n", $f);
+printf("    0.3: %.2f \n", 0.3);

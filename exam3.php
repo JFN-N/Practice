a0da67a98d20a5a6974a014001bc1bde90b3b995
EC2 Default User <staiju77@gmail.com>
2023-10-23 12:40:24 +0000

    勉強

--- exam3.php
+++ exam3.php
@@ -2,8 +2,16 @@
 
 $i = 123;
 
-if($i <= 100){
-    echo "100以下\n";
-}else{
-    echo "100以上\n";
+switch($i) {
+    case 0:
+        echo "i は 0です \n";
+        break;
+
+    case 1:
+        echo "i は 1です \n";
+        break;
+
+    default:
+        echo "i は 0と1 以外です \n";
+        break;
 }
